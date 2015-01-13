0aaee0463b57074a16547a0b742f041f1d53ee57
john ko <johnsko89@gmail.com>
2015-01-13 10:56:41 -0500

    creating controller and test for circle

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,29 +1,41 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'user signs in', %Q{
-  As a signed up user
-  I want to sign in
-  So that I can regain access to my account
-} do
-  scenario 'specify valid credentials' do
-    user = FactoryGirl.create(:user)
+feature "User Signs In", %q(
+As an unauthenticated user
+I want to sign in
+So that I can post items and review them
 
+Acceptance Criteria
+[ ] User must visit "/users/sign_in"
+[ ] User must enter valid email and password
+[ ] User will recieve an error if incorrect email/passord combo
+
+
+) do
+
+  let(:user){ FactoryGirl.create(:user) }
+
+  scenario 'user successfully signs in' do
     visit new_user_session_path
-
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-
-    click_button 'Log in'
-
-    expect(page).to have_content('Signed in successfully')
-    expect(page).to have_content('Sign Out')
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: user.password
+    click_button "Log in"
+    expect(page).to have_content("Signed in successfully")
   end
 
-  scenario 'specify invalid credentials' do
+  scenario 'user enters invalid email' do
     visit new_user_session_path
+    fill_in "user[email]", with: "notmyemail@email.com"
+    fill_in "user[password]", with: user.password
+    click_button "Log in"
+    expect(page).to have_content("Invalid email or password")
+  end
 
-    click_button 'Log in'
-    expect(page).to have_content('Invalid email or password')
-    expect(page).to_not have_content('Sign Out')
+  scenario 'user enters invalid password' do
+    visit new_user_session_path
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: "notmypassword"
+    click_button "Log in"
+    expect(page).to have_content("Invalid email or password")
   end
 end
