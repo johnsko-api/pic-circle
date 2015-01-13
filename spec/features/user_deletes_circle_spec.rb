8a44edd73a69b2407a26ea5c931cdd0ce52327c2
john ko <johnsko89@gmail.com>
2015-01-13 15:15:44 -0500

    fixed all tests and association in circles controller

--- spec/features/user_deletes_circle_spec.rb
+++ spec/features/user_deletes_circle_spec.rb
@@ -11,15 +11,15 @@ Acceptance Criteria
 
 ) do
 
-  let(:user){ FactoryGirl.create(:user) }
+  # let(:user){ FactoryGirl.create(:user) }
   let(:circle) { FactoryGirl.create(:circle) }
   context "authenticated user" do
 
-    scenario "author of buddy successfully edits circle" do
+    scenario "author of buddy successfully deletes circle" do
 
       visit new_user_session_path
-      fill_in "user[email]", with: user.email
-      fill_in "user[password]", with: user.password
+      fill_in "user[email]", with: circle.user.email
+      fill_in "user[password]", with: circle.user.password
 
       click_button "Log in"
       visit circle_path(circle)
@@ -27,6 +27,26 @@ Acceptance Criteria
       click_link "Delete"
       expect(page).to have_content("You've successfully deleted a circle!")
 
+    end
+  end
+  scenario "not the author of circle tries to delete circle" do
+    user2 = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in "user[email]", with: user2.email
+    fill_in "user[password]", with: user2.password
+    click_button "Log in"
+
+    visit circle_path(circle)
+    click_link "Delete"
+    expect(page).to have_content("You are not the owner of that circle!")
+  end
+
+  context "unauthenticated user" do
+    scenario "user tries to delete circle when not signed in" do
+      visit circle_path(circle)
+
+      click_link "Delete"
+      expect(page).to have_content("You need to sign in or sign up before continuing.")
     end
   end
 end
