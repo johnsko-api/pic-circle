995a224f4229afa5e45afb349ddf3cfd09635da5
john ko <johnsko89@gmail.com>
2015-01-13 13:08:23 -0500

    can add circle with testing

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,2 +1,35 @@
 class CirclesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @circles = Circle.all
+  end
+
+  def new
+    @circle = Circle.new
+  end
+
+  def create
+    @circle = Circle.new(circle_params)
+    if @circle.save
+      flash[:notice] = "You've successfully created a circle!"
+      redirect_to @circle
+    else
+      flash[:alert] = @circle.errors.full_messages
+      render "circles/show"
+    end
+  end
+
+  def show
+    @circle = Circle.find(params[:id])
+  end
+
+
+  private
+
+  def circle_params
+    params.require(:circle).permit(:title, :description)
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113153650) do
+ActiveRecord::Schema.define(version: 20150113161642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "circles", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.text     "description", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
