94fa712cf38d9b3c791664338939e4f086e3ad1b
john ko <johnsko89@gmail.com>
2015-01-23 02:07:17 -0500

    added one more validation

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -3,7 +3,7 @@ class Circle < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :pictures, dependent: :destroy
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
 
   validates :title, length: { maximum: 20 }, presence: true
   validates :description, presence: true
