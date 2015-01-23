11e11150eee503358572bb95d3aabc1dbd47b70f
john ko <johnsko89@gmail.com>
2015-01-23 16:35:54 -0500

    added validation for images

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,8 @@ class Picture < ActiveRecord::Base
   belongs_to :user
   belongs_to :circle
 
-  has_attached_file :image
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, presence: true
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/, presence: true
   validate :file_dimensions
 
 private
